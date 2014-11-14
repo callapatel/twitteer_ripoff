1167d75977a6f2fc3a679ccc68a78d709118e422
Calla <callapatel@gmail.com>
2014-11-14 11:00:43 -0800

    validation test for body 140 char

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-  validates :body, presence: true
+  validates :body, presence: true, length: {maximum:140}
 
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 
 describe Post do
+
   describe '.validates' do #class method is what the .indicated
     it 'body is present' do
       expect(Post.new.valid?).to eq false
-
     end
+
+    it 'body is fewer than 141 characters' do
+      expect(Post.new(body:"a" * 141).valid?).to eq false
+    end
+
   end
 end
